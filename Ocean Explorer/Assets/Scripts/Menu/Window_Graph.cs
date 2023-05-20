ebf0dc06a5f9ce977ebae1a99a424f8e6f3dc679
Rusu Eduard <rusu00eduard@gmail.com>
2023-05-20 15:42:46 +0300

    feat: added axis labels and separators

--- Ocean Explorer/Assets/Scripts/Menu/Window_Graph.cs
+++ Ocean Explorer/Assets/Scripts/Menu/Window_Graph.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using CodeMonkey.Utils;
+using TMPro;
 
 public class Window_Graph : MonoBehaviour
 {
@@ -14,9 +15,40 @@ public class Window_Graph : MonoBehaviour
 
     private List<List<float>> data = new List<List<float>>();
 
+    private List<GameObject> points = new List<GameObject>();
+    private List<GameObject> lines = new List<GameObject>();
+    private List<GameObject> labelsX = new List<GameObject>();
+    private List<GameObject> labelsY = new List<GameObject>();
+    private List<GameObject> dashesX = new List<GameObject>();
+    private List<GameObject> dashesY = new List<GameObject>();
+
+    private RectTransform labelTemplateX;
+    private RectTransform labelTemplateY;
+    private RectTransform dashTemplateX;
+    private RectTransform dashTemplateY;
+    private GameObject tooltipGameObject;
+
+    public Button minSpeedButton;
+    public Button maxSpeedButton;
+    public Button alignmentButton;
+    public Button cohesionButton;
+    public Button separationButton;
+    public Button avoidanceButton;
+
+    public int separatorCountX;
+    public int separatorCountY;
+
+    private Button disabledButton;
+
     private void Awake()
     {
         graphContainer = GetComponent<RectTransform>();
+        labelTemplateX = graphContainer.Find("labelTemplateX").GetComponent<RectTransform>();
+        labelTemplateY = graphContainer.Find("labelTemplateY").GetComponent<RectTransform>();
+        dashTemplateX = graphContainer.Find("dashTemplateY").GetComponent<RectTransform>();
+        dashTemplateY = graphContainer.Find("dashTemplateX").GetComponent<RectTransform>();
+        tooltipGameObject = graphContainer.Find("tooltip").gameObject;
+
         var fileData = System.IO.File.ReadAllLines(Application.dataPath + "/data.csv");
 
         foreach (var param in fileData[0].Trim().Split(','))
@@ -32,8 +64,65 @@ public class Window_Graph : MonoBehaviour
                 data[j].Add(float.Parse(lineData[j], CultureInfo.InvariantCulture.NumberFormat));
             }
         }
-        ShowGraph(data[(int)DataIndexingEnum.ALIGN_WEIGHT]);
+        ShowGraph(data[(int)DataIndexingEnum.MIN_SPEED]);
+        minSpeedButton.interactable = false;
+        disabledButton = minSpeedButton;
     }
+
+    public void ShowMinSpeed()
+    {
+        ClearGraph();
+        disabledButton.interactable = true;
+        minSpeedButton.interactable = false;
+        ShowGraph(data[(int)DataIndexingEnum.MIN_SPEED]);
+        disabledButton = minSpeedButton;
+    }
+
+    public void ShowMaxSpeed()
+    {
+        ClearGraph();
+        disabledButton.interactable = true;
+        maxSpeedButton.interactable = false;
+        ShowGraph(data[(int)DataIndexingEnum.MAX_SPEED]);
+        disabledButton = maxSpeedButton;
+    }
+
+    public void ShowAlignment()
+    {
+        ClearGraph();
+        disabledButton.interactable = true;
+        alignmentButton.interactable = false;
+        ShowGraph(data[(int)DataIndexingEnum.ALIGN_WEIGHT]);
+        disabledButton = alignmentButton;
+    }
+
+    public void ShowCohesion()
+    {
+        ClearGraph();
+        disabledButton.interactable = true;
+        cohesionButton.interactable = false;
+        ShowGraph(data[(int)DataIndexingEnum.COHESION_WEIGHT]);
+        disabledButton = cohesionButton;
+    }
+
+    public void ShowSeparation()
+    {
+        ClearGraph();
+        disabledButton.interactable = true;
+        separationButton.interactable = false;
+        ShowGraph(data[(int)DataIndexingEnum.SEPARATE_WEIGHT]);
+        disabledButton = separationButton;
+    }
+
+    public void ShowAvoidance()
+    {
+        ClearGraph();
+        disabledButton.interactable = true;
+        avoidanceButton.interactable = false;
+        ShowGraph(data[(int)DataIndexingEnum.AVOID_COLLISION_WEIGHT]);
+        disabledButton = avoidanceButton;
+    }
+
 
     private GameObject CreateCircle(Vector2 anchoredPosition)
     {
@@ -42,11 +131,16 @@ public class Window_Graph : MonoBehaviour
         gameObject.GetComponent<Image>().sprite = circleSprite;
         RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
         rectTransform.anchoredPosition = anchoredPosition;
-        rectTransform.sizeDelta = new Vector2(11, 11);
+        rectTransform.sizeDelta = new Vector2(5, 5);
         rectTransform.anchorMin = new Vector2(0, 0);
         rectTransform.anchorMax = new Vector2(0, 0);
 
         return gameObject;
+    }
+
+    private void HideTooltip()
+    {
+        tooltipGameObject.SetActive(false);
     }
 
     private void ShowGraph(List<float> valueList)
@@ -54,18 +148,80 @@ public class Window_Graph : MonoBehaviour
         float graphHeight = graphContainer.sizeDelta.y;
         float maxValue = valueList.Max();
         float minValue = valueList.Min();
-        GameObject lastGameObject = null;
+        int labelSpacerX = Round(valueList.Count / separatorCountX);
         for (int i = 0; i < valueList.Count; i++)
         {
             float xPosition = ((float)i / valueList.Count) * graphContainer.sizeDelta.x;
             float yPosition = (valueList[i] - minValue) / (maxValue - minValue) * graphHeight;
+
+            string tooltipText = valueList[i].ToString();
+
             GameObject circle = CreateCircle(new Vector2(xPosition, yPosition));
-            if (lastGameObject != null)
+            if (points.Count != 0)
             {
-                CreateDotConnection(lastGameObject.GetComponent<RectTransform>().anchoredPosition, circle.GetComponent<RectTransform>().anchoredPosition);
+                CreateDotConnection(points.Last().GetComponent<RectTransform>().anchoredPosition, circle.GetComponent<RectTransform>().anchoredPosition);
             }
-            lastGameObject = circle;
+            points.Add(circle);
+
+            if (i % labelSpacerX == 0)
+            {
+                CreateLabelX(xPosition, i.ToString());
+                CreateDashX(xPosition);
+            }
+
         }
+
+        for (int i = 0; i < separatorCountY; i++)
+        {
+            float normalizedValue = i * 1f / separatorCountY;
+            CreateLabelY(normalizedValue * graphHeight, (normalizedValue * maxValue + minValue).ToString());
+            CreateDashY(normalizedValue * graphHeight);
+        }
+    }
+
+    private int Round(int n)
+    {
+        int a = (n / 10) * 10;
+        int b = a + 10;
+        return (n - a > b - n) ? b : a;
+    }
+
+    private void CreateLabelX(float position, string text)
+    {
+        RectTransform labelX = Instantiate(labelTemplateX);
+        labelX.SetParent(graphContainer, false);
+        labelX.gameObject.SetActive(true);
+        labelX.anchoredPosition = new Vector2(position, -7f);
+        labelX.GetComponent<Text>().text = text;
+        labelsX.Add(labelX.gameObject);
+    }
+
+    private void CreateDashX(float position)
+    {
+        RectTransform dashX = Instantiate(dashTemplateX);
+        dashX.SetParent(graphContainer, false);
+        dashX.gameObject.SetActive(true);
+        dashX.anchoredPosition = new Vector2(position, 0);
+        dashesX.Add(dashX.gameObject);
+    }
+
+    private void CreateDashY(float position)
+    {
+        RectTransform dashY = Instantiate(dashTemplateY);
+        dashY.SetParent(graphContainer, false);
+        dashY.gameObject.SetActive(true);
+        dashY.anchoredPosition = new Vector2(0, position);
+        dashesY.Add(dashY.gameObject);
+    }
+
+    private void CreateLabelY(float position, string text)
+    {
+        RectTransform labelY = Instantiate(labelTemplateY);
+        labelY.SetParent(graphContainer, false);
+        labelY.gameObject.SetActive(true);
+        labelY.anchoredPosition = new Vector2(-7f, position);
+        labelY.GetComponent<Text>().text = text;
+        labelsY.Add(labelY.gameObject);
     }
 
     private void CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB)
@@ -81,5 +237,37 @@ public class Window_Graph : MonoBehaviour
         rectTransform.anchorMax = new Vector2(0, 0);
         rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
         rectTransform.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(dir));
+        lines.Add(gameObject);
+    }
+
+    private void ClearGraph()
+    {
+        foreach (var point in this.points)
+        {
+            Destroy(point);
+        }
+        foreach (var line in this.lines)
+        {
+            Destroy(line);
+        }
+
+        for (int i = 0; i < separatorCountX; i++)
+        {
+            Destroy(this.labelsX[i]);
+            Destroy(this.dashesX[i]);
+        }
+
+        for (int i = 0; i < separatorCountY; i++)
+        {
+            Destroy(this.labelsY[i]);
+            Destroy(this.dashesY[i]);
+        }
+
+        this.points.Clear();
+        this.lines.Clear();
+        this.labelsX.Clear();
+        this.labelsY.Clear();
+        this.dashesX.Clear();
+        this.dashesY.Clear();
     }
 }
